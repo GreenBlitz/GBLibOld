f8dc6c6eefb1212aa967ffebbf78442e7364b795
GBSoftwareLaptop <greenblitzfrc@gmail.com>
2020-01-08 18:43:46 +0200

    I hate my life

--- src/main/java/edu.greenblitz.gblib/sensors/PressureSensor.java
+++ src/main/java/edu.greenblitz.gblib/sensors/PressureSensor.java
@@ -1,18 +1,18 @@
-package edu.greenblitz.gblib.sensors;
-
-import edu.wpi.first.wpilibj.AnalogInput;
-
-public class PressureSensor extends AnalogInput {
-
-    public PressureSensor(int channel) {
-        super(channel);
-    }
-
-    public double getPressure() {
-        return Math.max(0, (250 * getVoltage() / 5) - 29/*25 (Through testing, subtracted 4 from the original formula)*/); 
-    }
-
-    public double getAveragePressure() {
-        return Math.max(0, (250 * getAverageVoltage() / 5) - 29/*25 (Through testing, subtracted 4 from the original formula)*/);
-    }
-}
+//package edu.greenblitz.gblib.sensors;
+//
+//import edu.wpi.first.wpilibj.AnalogInput;
+//
+//public class PressureSensor extends AnalogInput {
+//
+//    public PressureSensor(int channel) {
+//        super(channel);
+//    }
+//
+//    public double getPressure() {
+//        return Math.max(0, (250 * getVoltage() / 5) - 29/*25 (Through testing, subtracted 4 from the original formula)*/);
+//    }
+//
+//    public double getAveragePressure() {
+//        return Math.max(0, (250 * getAverageVoltage() / 5) - 29/*25 (Through testing, subtracted 4 from the original formula)*/);
+//    }
+//}
